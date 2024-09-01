61dfdc0749847dd438464c21264c796eb8caa666
terance-edmonds <teranceedmonds20@gmail.com>
2024-09-01 17:17:46 +0530

    home page carousel

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import Home from './pages/home';
 
 function App() {
     return (
-        <div className="flex justify-center bg-[#28282B] w-full min-h-screen relative">
+        <div className="flex justify-center bg-[#28282B] w-full min-h-screen">
             <div className="w-full min-h-screen max-w-[550px] bg-black flex flex-col gap-3">
                 <TopNavbar />
 
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                </Routes>
+                <main className="h-full">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </main>
 
                 <BottomNavbar />
             </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,44 @@
 import SearchInput from '@/components/search';
+// @ts-ignore
+import ReactCardCarousel from 'react-card-carousel';
 
 export default function Home() {
     return (
-        <div className="flex flex-col gap-4 min-h-screen">
+        <div className="flex flex-col gap-4 relative min-h-[80vh]">
             <SearchInput />
+
+            <div className="relative h-[75vh]">
+                <ReactCardCarousel>
+                    {Array.from({ length: 10 }).map((_, i: number) => {
+                        return (
+                            <div
+                                key={i}
+                                className="w-[336px] h-[550px] rounded-2xl overflow-hidden relative bg-black">
+                                <img
+                                    src="https://picsum.photos/400/550"
+                                    alt="Sample"
+                                    className="object-cover w-full h-full"
+                                />
+
+                                <div
+                                    className="text-white absolute top-0 left-0 px-5 pt-5 pb-12 text-center h-full flex flex-col justify-end"
+                                    style={{
+                                        background:
+                                            'linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.4351514287355567) 80%)'
+                                    }}>
+                                    <h1 className="text-inherit font-bold text-2xl mb-2">
+                                        Negombo
+                                    </h1>
+                                    <p className="text-inherit">
+                                        A coastal city known for its sandy beaches and bustling fish
+                                        market
+                                    </p>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </ReactCardCarousel>
+            </div>
         </div>
     );
 }
